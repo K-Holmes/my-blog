839cb05ac820d0af25b2e819a445e44dea0d88fd
K-Holmes <Kayla.Holmes@cdpipelinedevshop.com>
2022-07-19 02:46:45 +0000

    Blank screen, need to fix

---
+++ my-blog/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Blogged from './Components/Blog';
+
+function App() {
+  return (
+    <div className="App">
+      <Blogged></Blogged>
+    </div>
+  );
+}
+
+export default App;
